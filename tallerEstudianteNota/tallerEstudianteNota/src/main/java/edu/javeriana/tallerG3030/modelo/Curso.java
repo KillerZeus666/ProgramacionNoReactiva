069a82c7620f3aba1142e45d2dfeb8735e387adb
Katheryn Guasca <katheryng2017@gmail.com>
2024-11-02 13:37:26 +0000

    Agregando clase

---
+++ tallerEstudianteNota/tallerEstudianteNota/src/main/java/edu/javeriana/tallerG3030/modelo/Curso.java
@@ -0,0 +1,30 @@
+package edu.javeriana.tallerG3030.modelo;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "curso")
+public class Curso implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "materia_id", nullable = false)
+    private Materia materia;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "profesor_id", nullable = false)
+    private Profesor profesor;
+
+    private String codigo;
+    private Date fechaInicio;
+    private Date fechaFin;
+}
--- tallerEstudianteNota/tallerEstudianteNota/src/main/java/edu/javeriana/tallerG3030/modelo/Profesor.java
+++ tallerEstudianteNota/tallerEstudianteNota/src/main/java/edu/javeriana/tallerG3030/modelo/Profesor.java
@@ -9,11 +9,12 @@ import java.io.Serializable;
 @Setter
 @Getter
 @Entity
-@Table(name = "materia")
-public class Materia implements Serializable {
+@Table(name = "profesor")
+public class Profesor implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
-    private int creditos;
+    private String apellido;
+    private String correo;
 }
